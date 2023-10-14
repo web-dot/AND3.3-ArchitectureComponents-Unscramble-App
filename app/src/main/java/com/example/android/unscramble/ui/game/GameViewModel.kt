bf36053acdf7c1af9d16716c9880f58f7d92c0b8
sudipto <cg.sudipc@gmail.com>
2023-10-14 21:52:48 +0530

    populated getNextWord() in GameViewModel to get a scramblled word when the app starts

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,13 +5,34 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
-    init {
-        Log.d("Game Fragment", "GameViewModel created")
-    }
 
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord(){
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+
+        while (String(tempWord).equals(currentWord, false)){
+            tempWord.shuffle()
+        }
+
+        if(wordsList.contains(currentWord)){
+            getNextWord()
+        }
+        else{
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+    init {
+        Log.d("GameFragment", "GameViewModel created")
+        getNextWord()
+    }
 
     // backing property
     val currentScrambledWord: String
@@ -21,4 +42,5 @@ class GameViewModel: ViewModel() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed")
     }
+
 }
